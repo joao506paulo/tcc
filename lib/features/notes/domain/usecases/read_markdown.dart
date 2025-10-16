7dc424d083b7bf1932afc5d2df524a28c60eb0dc
joao506paulo <joao506paulo@gmail.com>
2025-10-16 17:35:29 -0300

    Add files via upload

---
+++ lib/features/notes/domain/usecases/read_markdown.dart
@@ -0,0 +1,7 @@
+import '../../../core/usecases/usecase.dart';
+class ReadMarkdown extends UseCase<void, String> {
+  @override
+  Future<void> call(String params) async {
+    // TODO: implement use case
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,3 @@
+void main() {
+  // Entry point of the Flutter app
+}
